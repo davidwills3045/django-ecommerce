aaf09b42b1327ceadce3514020d23253e789c3e1
davidwills3045 <davidonaijaflavour612@gmail.com>
2023-09-22 13:16:42 +0100

    second commit

--- ecommerce/ecommerce_project/userauths/urls.py
+++ ecommerce/ecommerce_project/userauths/urls.py
@@ -3,4 +3,5 @@ from .import views
 
 urlpatterns = [
     path('signup/',views.register_view,name='sign-up'),
+    path('signin/',views.login_view,name='sign-in'),
 ]
--- ecommerce/ecommerce_project/userauths/views.py
+++ ecommerce/ecommerce_project/userauths/views.py
@@ -1,6 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from .forms import UserRegisterForm
+from django.contrib.auth import login,authenticate
+from django.contrib import messages
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
 
 
 def register_view(request):
-    return render(request, "core/sign-up.html")
+
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST or None)
+        if form.is_valid():
+            new_user = form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(request, f"Hey {username}, Your account was successfuly created")
+            new_user = authenticate(username=form.cleaned_data['email'],password= form.cleaned_data['password1'])
+            login(request, new_user)
+            return redirect("index") 
+    else:
+        form = UserRegisterForm()
+
+    context = {
+        'form':form
+    }
+    return render(request, "userauths/sign-up.html",context)
+
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect("index")
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        try:
+            user = User.objects.get(email=email)
+        except:
+            messages.warning(request,f"User with {email} does not exist")
+
+        user = authenticate(request,email=email,password=password)
+
+        if user is not None:
+            login(request,user)
+            messages.success(request, "You are logged in.")
+            return redirect("index")
+        else:
+            messages.warning(request, "User Does Not Exist, Create an account.")
+    context = {
+
+    }
+    return render(request,"userauths/sign-in.html",context)
+
+    
